0d80f795b53b245a86548076581d856b464dd0d1
jamili <jamili.amaral@estudante.ifms.edu.br>
2023-09-27 00:05:33 -0400

    atualizar

--- js/db.js
+++ js/db.js
@@ -32,6 +32,7 @@ window.addEventListener('DOMContentLoaded', async event =>{
     document.getElementById('btnCarregar').addEventListener('click', buscarTodasAnotacoes);
     document.getElementById('btnBuscar').addEventListener('click', buscarAnotacao);
     document.getElementById('btnRemover').addEventListener('click', removerAnotacao);
+    document.getElementById('btnAtualizar').addEventListener('click', atualizarAnotacao);
 });
 
 async function buscarAnotacao() {
@@ -83,6 +84,12 @@ async function adicionarAnotacao() {
     let descricao = document.getElementById("descricao").value;
     let data = document.getElementById("data").value;
     let categoria = document.getElementById("categoria").value;
+
+    if (!titulo || !descricao || !data) {
+        console.log('Preencha todos os campos obrigatórios (Título, Descrição e Data).');
+        return;
+    }
+    
     const tx = await db.transaction('anotacao', 'readwrite')
     const store = tx.objectStore('anotacao');
     try {
@@ -114,6 +121,61 @@ async function removerAnotacao() {
         tx.abort();
     }
 }
+async function atualizarAnotacao() {
+    const tituloParaAtualizar = prompt('Digite o título da anotação que deseja atualizar:');
+    if (!tituloParaAtualizar) {
+        console.log('Operação de atualização cancelada.');
+        return;
+    }
+    const tx = await db.transaction('anotacao', 'readwrite');
+    const store = tx.objectStore('anotacao');
+    const anotacaoExistente = await store.get(tituloParaAtualizar);
+    if (!anotacaoExistente) {
+        console.log('Anotação não encontrada.');
+        return;
+    }
+
+    // Solicite ao usuário que atualize os campos desejados
+    const novaDescricao = prompt('Nova descrição:');
+    const novaData = prompt('Nova data (no formato yyyy-mm-dd):');
+    
+    // Solicite ao usuário que selecione uma nova 
+    const categoriasExistentes = ['trabalho', 'estudos', 'pessoal', 'academia', 'casa', 'formula 1'];
+    const novaCategoria = prompt(`Nova categoria (${categoriasExistentes.join(', ')}):`);
+
+    // Validar a data no formato yyyy-mm-dd
+    if (novaData && !/^\d{4}-\d{2}-\d{2}$/.test(novaData)) {
+        console.log('Formato de data inválido. Use o formato yyyy-mm-dd.');
+        return;
+    }
+
+    // Validar se a nova categoria está entre as categorias existentes
+    if (novaCategoria && !categoriasExistentes.includes(novaCategoria)) {
+        console.log('Categoria inválida. Escolha uma das categorias existentes.');
+        return;
+    }
+
+    // Atualize os campos na anotação existente
+    if (novaDescricao) {
+        anotacaoExistente.descricao = novaDescricao;
+    }
+    if (novaData) {
+        anotacaoExistente.data = novaData;
+    }
+    if (novaCategoria) {
+        anotacaoExistente.categoria = novaCategoria;
+    }
+    try {
+        await store.put(anotacaoExistente);
+        await tx.done;
+        console.log('Anotação atualizada com sucesso!');
+        buscarTodasAnotacoes();
+    } catch (error) {
+        console.error('Erro ao atualizar a anotação:', error);
+        tx.abort();
+    }
+}
+
 
 function limparCampos() {
     document.getElementById("titulo").value = '';
